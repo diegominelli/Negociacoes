a4417f898e96acae992abb989e6d49ed624b6ef8
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-05-28 18:23:26 -0300

    finalizando o modelo

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,4 +2,4 @@ import { Negociacao } from "./models/negociacao.js";
 
 const negociacao = new Negociacao(new Date(), 10, 100);
 
-negociacao.quantidade = 1000;
+console.log(negociacao.volume);
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -7,6 +7,21 @@ export class Negociacao {
     this.#data = data;
     this.#quantidade = quantidade;
     this.#valor = valor;
+  }
 
+  get data() {
+    return this.#data;
+  }
+
+  get quantidade() {
+    return this.#quantidade;
+  }
+
+  get valor() {
+    return this.#valor;
+  }
+
+  get volume() {
+    return this.#quantidade * this.#valor;
   }
 }
