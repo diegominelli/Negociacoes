c42a995c7b3dbae0a036e2467781fcbc38291801
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-09 19:31:52 -0300

    usando o Object.defineProperty

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,8 +1,13 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        console.log(`Modificando o prototype ${target.constructor.name} 
+      e adicionando getter para a propriedade ${propertyKey}`);
         const getter = function () {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elementodo DOM com o seletor 
+        ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         };
+        Object.defineProperty(target, propertyKey, { get: getter });
     };
 }
