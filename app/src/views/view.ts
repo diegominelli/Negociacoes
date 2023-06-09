830c4a9182c9ace47c86d212b434090484f59801
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-09 18:34:21 -0300

    simplificando o decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -20,8 +20,8 @@ export abstract class View<T> {
     }
   }
 
-  @inspect()
   @logarTempoDeExecucao(true)
+  @inspect
   public update(model: T): void {
     let template = this.template(model);
 
